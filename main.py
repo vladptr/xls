5954ce197e2595b03dc35c680570fa8d16afd4d8
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2025-08-07 23:09:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ui import View, Select
 from discord.ui import View, Button
 import random
@@ -909,6 +909,17 @@ def get_next_level_exp(level: int) -> int:
     else:
         return 1875 + 150 * (level - 25)
 
+def get_next_level_exp(level):
+    return 50 + (level - 1) * 25
+
+def calculate_level_from_exp(exp):
+    level = 1
+    total = 0
+    while exp >= get_next_level_exp(level):
+        total += get_next_level_exp(level)
+        level += 1
+    return level, exp - total, get_next_level_exp(level)
+
 def get_total_exp_before(level):
     total = 0
     for l in range(1, level):
@@ -1008,7 +1019,12 @@ async def stat(ctx, member: discord.Member = None):
 
         filename = f"stat_{user_id}.png"
         img.save(filename)
-        await ctx.send(file=discord.File(filename))
+        stat_msg = await ctx.send(file=discord.File(filename))
+
+        # Удаление через 3 минуты
+        await asyncio.sleep(180)
+        await stat_msg.delete()
+        os.remove(filename)
 
     except Exception as e:
         await ctx.send(f"❌ Ошибка в команде stat: {e}")
@@ -1032,6 +1048,18 @@ async def setexp(ctx, member: discord.Member = None):
     except Exception as e:
         await ctx.send(f"❌ Ошибка при начислении опыта: {e}")
 
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def resetstat(ctx, member: discord.Member):
+    try:
+        user_id = member.id
+
+        # Обнуляем опыт
+        supabase.table("user_levels").upsert({"user_id": user_id, "exp": 0}).execute()
+        await ctx.send(f"🔁 Статистика пользователя {member.mention} сброшена.")
+
+    except Exception as e:
+        await ctx.send(f"❌ Ошибка при сбросе: {e}")
 #//////////////////////////////////////
 AUTHORIZED_USER_ID = 455023858463014922
 
