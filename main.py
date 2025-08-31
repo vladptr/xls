ff9d606e65e3035a70639da76cbdbe8b3cecbab9
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2025-08-31 22:17:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -993,11 +993,13 @@ def get_next_level_exp(level: int) -> int:
 
 def calculate_level_from_exp(exp):
     level = 1
-    total = 0
-    while exp >= get_next_level_exp(level):
-        total += get_next_level_exp(level)
+    while True:
+        need = get_next_level_exp(level)
+        if exp < need:
+            break
+        exp -= need
         level += 1
-    return level, exp - total, get_next_level_exp(level)
+    return level, exp, get_next_level_exp(level)
 
 def get_total_exp_before(level):
     total = 0
