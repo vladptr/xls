b204e01579ec9fb07c7b87ae58cddc5982de7f53
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2025-12-27 17:37:44 +0200

    Update check.py

--- check.py
+++ check.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 
 async def setup(bot: commands.Bot):
 
-    @bot.command(name="check")
+    @bot.command()
     async def check(ctx):
         await ctx.send("1")
+
