3e20f96e3217473bc41f0027ae9d8d7248ce657d
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2026-02-12 11:17:18 +0200

    Configure Discord intents and voice room triggers
    Added Discord intents and configuration for voice room triggers.

--- modules/config.py
+++ modules/config.py
@@ -1,22 +1,41 @@
 import os
+import discord
 import asyncio
 
-# PUBG API
-PUBG_API_KEY = os.getenv("PUBG_API_KEY", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjZmMyNDMyMC01NzZlLTAxM2UtMjAyNS0yYTI4ZjY0MjU0ZDEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNzU0NzU4MTk5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InhsczIifQ.C74qapztROZBtCVEWdob2w4B0-omdLJ-aaBfdfFK91E")
-PUBG_PLATFORM = os.getenv("PUBG_PLATFORM", "steam")
-print("PUBG API key:", repr(PUBG_API_KEY))
+# -------------------------------
+# PUBG
+# -------------------------------
+PUBG_API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjZmMyNDMyMC01NzZlLTAxM2UtMjAyNS0yYTI4ZjY0MjU0ZDEiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNzU0NzU4MTk5LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InhsczIifQ.C74qapztROZBtCVEWdob2w4B0-omdLJ-aaBfdfFK91E"
+PUBG_PLATFORM = "steam"
 
-# Discord
+# -------------------------------
+# Discord Intents
+# -------------------------------
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+intents.voice_states = True
+intents.messages = True
+intents.message_content = True
+
+# -------------------------------
+# Blacklisted channels
+# -------------------------------
 BLACKLISTED_CHANNELS = {
     1187507350156886096,
     848713620959002684,
 }
 
-LEADERBOARD_CHANNEL_ID = 1371926685435428927
-
-# Голосовые каналы
+# -------------------------------
+# Voice room triggers
+# -------------------------------
 TRIGGER_CHANNELS = {
     "🔴・Создать ранкед руму": {"base": "🏆・Ранкед рума", "category": "Ранкед🔴"},
     "🔴・Создать паблик руму": {"base": "🟢・Паблик рума", "category": "Паблик🔴"},
     "🔴・Создать кастомную комнату": {"base": "🎮・Кастом игра", "category": "Кастомки🔴"}
 }
+
+# -------------------------------
+# Leaderboard
+# -------------------------------
+LEADERBOARD_CHANNEL_ID = 1371926685435428927
