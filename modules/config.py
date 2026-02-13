bcb0054efaf0bcd517e455c394c1c5b4183c0aaf
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2026-02-13 14:15:46 +0200

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -37,11 +37,12 @@ TRIGGER_CHANNELS = {
 AUTHORIZED_USER_ID = 455023858463014922
 
 # Main Guild ID - берется из переменной окружения или использует значение по умолчанию
-MAIN_GUILD_ID = int(os.getenv("MAIN_GUILD_ID", "520183812148166656"))
+MAIN_GUILD_ID = int(os.getenv("MAIN_GUILD_ID", "1371926685435428924"))
 
 # FFmpeg setup (только для Linux/Mac, на Windows пропускается)
 try:
     subprocess.run(["chmod", "+x", "./ffmpeg"], check=False)
 except:
     pass  # Игнорируем ошибку на Windows
+
 
