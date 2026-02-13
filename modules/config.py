11453441ce6b40a28716a9676eff72db025b87b7
VladPetrenko <59024535+vladptr@users.noreply.github.com>
2026-02-13 13:52:23 +0200

    Modify MAIN_GUILD_ID to read from environment variable
    Update MAIN_GUILD_ID to use environment variable.

--- modules/config.py
+++ modules/config.py
@@ -36,8 +36,8 @@ TRIGGER_CHANNELS = {
 # Authorized user ID
 AUTHORIZED_USER_ID = 455023858463014922
 
-# Main Guild ID
-MAIN_GUILD_ID = 520183812148166656
+# Main Guild ID - берется из переменной окружения или использует значение по умолчанию
+MAIN_GUILD_ID = int(os.getenv("MAIN_GUILD_ID", "520183812148166656"))
 
 # FFmpeg setup (только для Linux/Mac, на Windows пропускается)
 try:
